b0d2ba36a469bd1f80ca6e3e31731f5bfd299cca
James Reese <jreese42.vt.edu>
2013-04-15 15:07:01 -0400

    Add stock class in stock.py

---
+++ stock.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+import random as rand
+from trends import *
+class Stock:
+	"""Stock class used in generating and simulating fake stocks"""
+	scale = 1
+	price = 0
+	change = 0
+	trend_current = 1
+	trend_prev = 1
+	trend_status = 0
+	trend_exit = 4
+	trend_steady_state = 0
+	prev_price = 0
+	
+	trend_die = {1: trend_steady,
+		     2: trend_gen_rise,
+		     3: trend_gen_fall,
+		     4: trend_gr_rise,
+		     5: trend_gr_fall}
+
+	with open("name_prefix") as f:
+		name1 = f.read().splitlines()
+	with open("name_suffix") as f:
+		name2 = f.read().splitlines()
+
+	def __init__(self, scale_val = 1):
+		self.scale = scale_val
+		self.name = self.name1[rand.randint(0,len(self.name1)-1)] + " " + self.name2[rand.randint(0,len(self.name2)-1)]
+		self.price = 10*scale_val
+		rand.seed()
+		self.show()
+
+	def simulate(self):
+		#This function will simulate one day's change in a stock
+		self.prev_price = self.price
+
+		if (self.trend_status < self.trend_exit): #if in the middle of a trend already, continue
+			self.trend_die[self.trend_current](self) #execute a trend to simulate one day
+			self.trend_status += 1
+		elif (self.trend_status == self.trend_exit): #if at and of trend, pick new trend and simulate
+			#Reroll for a new trend
+			prev = self.trend_current
+			self.trend_status = 0
+			while (self.trend_current == prev):
+				self.trend_current = rand.randint(1,5)
+			#Execute the newly rolled trend
+			self.trend_die[self.trend_current](self)
+
+		#keep track of change between days
+		self.change = self.price - self.prev_price
+
+	def show(self):
+		#This function will print the name, price, and day's change of
+		#the stock to stdout
+		string = ""
+		string += self.name + ", "
+		string += "$" + str(self.price) + ", "
+		if (self.change < 0):
+			string += "-"
+		else:
+			string += "+"
+		string += "$" + str(abs(self.change))
+		print string
